2495950b43f706b11d15a2d5abc29ab53bb9b12a
NHeadson <nheadson@my.wctc.edu>
2026-05-04 23:24:10 -0500

    Fix linting issues and config

--- app/app.vue
+++ app/app.vue
@@ -3,5 +3,9 @@
     <h1 class="text-3xl font-bold underline">
       Hello world!
     </h1>
+    <button class="btn btn-secondary">
+      Secondary
+      <Icon name="tabler:bell" size="18" />
+    </button>
   </div>
 </template>
